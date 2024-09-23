4d9e5fd144977c0555b1e15fbc55994f7a48b61d
Gustavo Oliveira <gustax.dev@gmail.com>
2024-09-23 00:44:16 -0300

    20/09 - Substituição ScrollView por FlatList

--- app/Componentes/Adaptadores/ListaProdutos/index.tsx
+++ app/Componentes/Adaptadores/ListaProdutos/index.tsx
@@ -1,28 +1,22 @@
-import { ScrollView, View } from "react-native";
+import { FlatList } from "react-native";
 import Produto from "@/app/Models/Produto";
 import ItemProduto from "../ItemProduto";
-import Style from "@/app/Styles/Default";
 import React from "react";
 
 const ListaProdutos: React.FC<{
   produtos: Produto[];
   aoAtualizar?: Function;
-}> = ({ produtos, aoAtualizar }) => {
-  return (
-    <ScrollView>
-      <View style={Style.container}>
-        {produtos.map((p) => (
-          <ItemProduto
-            produto={p}
-            key={p.id}
-            aoExcluir={() => {
-              aoAtualizar?.call(null);
-            }}
-          />
-        ))}
-      </View>
-    </ScrollView>
-  );
-};
+}> = ({ produtos, aoAtualizar }) => (
+  <FlatList
+    data={produtos}
+    keyExtractor={(p) => p.id.toString()}
+    renderItem={({ item }) => (
+      <ItemProduto
+        produto={item}
+        aoExcluir={() => aoAtualizar?.call(null)}
+      ></ItemProduto>
+    )}
+  />
+);
 
 export default ListaProdutos;
